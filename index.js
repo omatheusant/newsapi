0e76f1512ac029736573adb4546a53c42132caa2
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-10 15:13:01 -0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require ('express');
-const server = express();
+const app = express();
 const astroNews = require ('./src/data/astroNews.json')
 
-server.get('/', (req, res) => {
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
+
+app.get('/', (req, res) => {
     return res.json(astroNews)
 })
 
-server.listen(5000, () => {
+app.listen(5000, () => {
     console.log('servidor está funcionando...')
 })
 
-module.exports = server
+module.exports = app
